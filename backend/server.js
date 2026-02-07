54d5bda782588a3e0d2d77d7412b2dcc05475664
amineBoukhari <amine.boukhari@tsplus.net>
2026-02-07 17:58:52 +0100

    adding pg Admin && connecting db with backend && seting up backend structure

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const sequelize = require('./src/db/index');
 
 app.use(cors({ origin: "http://127.0.0.1:5173" }));
 
 const port = process.env.PORT || 3000
+
+
+async function startServer (){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    if (process.env.NODE_ENV === 'dev'){
+      await sequelize.sync({ force: true });
+      console.log('All models were synchronized successfully.'); 
+    }
+  }
+  catch (error) {
+    console.error('Error starting server:', error);
+  }
+}
+
+startServer();
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!')
---
+++ backend/src/modules/user/user.model.js
@@ -0,0 +1,28 @@
+const sequelize = require('../../db/index');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    timestamps: true, 
+});
+
+module.exports = User;
