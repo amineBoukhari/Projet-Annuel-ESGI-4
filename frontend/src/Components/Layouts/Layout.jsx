e90df884192d686def18a437c4c2313c45747e0a
SpacioRift <louis.majchrzaj73@gmail.com>
2026-03-25 09:26:36 +0100

    Ajout CSS conditionnel
    Ajout class hidden si side menu closed

--- frontend/src/Components/Layouts/Layout.jsx
+++ frontend/src/Components/Layouts/Layout.jsx
@@ -1,17 +1,25 @@
 import { Outlet } from "react-router";
 import Header from "../Shared/Header";
+import { useState } from "react";
 
 export default function Layout(){
+
+const [open, setOpen] = useState(false);
+
+const handleSideMenu = () => {
+    setOpen(!open);
+}
+
     return (
-        <div className="p-3 flex gap-3 h-full">
-            <div className="side-menu w-1/3">
+        <div className={"p-3 flex gap-3 h-full"}>
+            <div className={open? "side-menu w-1/3" : "hidden"} >
             
-                <aside className="bg-white rounded-2xl h-full">
+                <aside className={open? "bg-white rounded-2xl h-full" : "hidden"}>
                     {/* TODO: ADD SIDE MENU */}
                 </aside>
             </div>
             <div className="hero-menu w-full h-full flex flex-col gap-3">
-                <Header />
+                <Header setOpenMenu={handleSideMenu}/>
                 <main className="content h-full w-full rounded-2xl">
                     <Outlet /> 
                 </main>
