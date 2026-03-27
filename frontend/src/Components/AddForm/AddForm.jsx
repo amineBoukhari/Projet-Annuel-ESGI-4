6fbdd8528ce0822403730831bb76a5183c7c1582
SpacioRift <louis.majchrzaj73@gmail.com>
2026-03-27 19:11:15 +0100

    Btn pour fermer la modal

--- frontend/src/Components/AddForm/AddForm.jsx
+++ frontend/src/Components/AddForm/AddForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'; 
+import { X } from 'lucide-react';
 
 export default function AddForm({ template, btnTitle }){
     
@@ -17,7 +18,11 @@ export default function AddForm({ template, btnTitle }){
 
                 {open&&
                     <div className='modal-form bg-transparent backdrop-blur-xs  flex justify-center items-center fixed inset-0 z-9999'>
-                        <div className='bg-white border-gray-300 border-1 rounded-lg shadow-xl w-[60%] h-[80%]'>
+                        <div className='bg-white border-gray-300 border-1 rounded-lg shadow-xl w-[60%] h-[80%] p-4'>
+                            <div className='bg-yellow-100 w-full h-15 flex flex-row justify-between items-center'>
+                                <p className='pl-4'>{btnTitle}</p>
+                                <X className=' size-10 pr-4' onClick={() => handleModal()} />
+                            </div>
                                                                                                 
                         </div>
                     </div>
