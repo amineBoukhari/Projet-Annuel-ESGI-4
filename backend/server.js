2887408cd6e93b60c8492f3143847a90235ae312
amineBoukhari <amine.boukhari@tsplus.net>
2026-07-10 16:48:01 +0200

    white list 80 port

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ Object.values(models).forEach((model) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://164.132.103.93:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://164.132.103.93:5173", "http://164.132.103.93"],
     credentials: true,
   }),
 );
